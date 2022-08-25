1505f05dfa9ae7462f827f1bd44d6a6fb79d2e3a
Mendy <mendyw94@gmail.com>
2022-08-25 17:40:47 -0400

    Completed Lab

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+{/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+        <h2>About</h2>
+    </div>
+  )
+}
+
+export default About
--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,9 @@
 import React from "react";
+import NavBar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
 
 
 function App() {
@@ -24,9 +11,13 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home/>
       {/* add your <About> component here */}
+      <About/>
     </div>
   );
 }
+
+// export {About};
 
 export default App;
